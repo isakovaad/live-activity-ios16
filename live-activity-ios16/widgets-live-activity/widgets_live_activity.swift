01cdc24c1f4ee0f7519e15c06faa0e57efb0c2e8
Dilbar Isakova <geekwarns@gmail.com>
2023-10-01 22:43:32 +0200

    updates

--- live-activity-ios16/widgets-live-activity/widgets_live_activity.swift
+++ live-activity-ios16/widgets-live-activity/widgets_live_activity.swift
@@ -5,26 +5,26 @@
 //  Created by eesaack on 18/08/2023.
 //
 
-import ActivityKit
-import WidgetKit
-import SwiftUI
-import Intents
-
-struct TimerActivity: View {
-    let context: ActivityViewContext<TimerActivity>
-    
-    var body: some View {
-        VStack
-    }
-}
-
-@main
-struct widgets_live_activity: Widget {
-    let kind: String = "widgets_live_activity"
-
-    var body: some WidgetConfiguration {
-        ActivityConfiguration(attributesType: TimerAttributes.self) { context in
-            
-        }
-    }
-}
+//import ActivityKit
+//import WidgetKit
+//import SwiftUI
+//import Intents
+//
+//struct TimerActivity: View {
+//    let context: ActivityViewContext<TimerActivity>
+//
+//    var body: some View {
+//        VStack
+//    }
+//}
+//
+//@main
+//struct widgets_live_activity: Widget {
+//    let kind: String = "widgets_live_activity"
+//
+//    var body: some WidgetConfiguration {
+//        ActivityConfiguration(attributesType: TimerAttributes.self) { context in
+//
+//        }
+//    }
+//}
